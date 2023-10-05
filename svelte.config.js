b691bd29c4331b61ec4492cc96cb0bff1545b98f
Siddhesh Mangela <sidthecool007@gmail.com>
2023-10-05 11:52:43 +0530

    path removed

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,9 +14,6 @@ const config = {
     adapter: adapter({
       fallback: 'index.html',
     }),
-    paths: {
-      base: dev ? '' : 'phrasebook',
-    },
     alias: {
       '~/*': 'src/*',
     },
