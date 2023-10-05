7b3e46accd9150fd887a115e3236a7d1c0b91814
Siddhesh Mangela <sidthecool007@gmail.com>
2023-10-05 13:46:55 +0530

    service worker added

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
+/// <reference types="vite-plugin-pwa/info" />
+
 declare global {
   namespace App {
     // interface Error {}
