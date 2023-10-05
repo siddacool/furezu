1af2ce232ff6c4f5f1863f06508cbdb209359cb3
Siddhesh Mangela <sidthecool007@gmail.com>
2023-10-05 12:18:46 +0530

    netlify added

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,11 +12,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback: 'index.html',
+      edge: false,
+      split: false,
     }),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-    },
     alias: {
       '~/*': 'src/*',
     },
