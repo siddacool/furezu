244234b4624efd45898f89aea801bd51d710e0bc
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-16 16:25:54 +0530

    color added

---
+++ src/lib/components/UI/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" module>
+  export type ButtonColorType = 'default' | 'primary' | 'danger';
+</script>
+
+<script lang="ts">
+  export let color: ButtonColorType = 'default';
+</script>
+
+<button class="Button color color--{color}"><slot /></button>
+
+<style lang="scss">
+  button {
+    margin: 0;
+    padding: 8px 5px;
+    outline: 0;
+    border: 1px solid;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import Button from '$lib/components/UI/Button.svelte';
+</script>
+
 <title>Yo</title>
 <h1>Welcome to SvelteKit 🇹🇭🇹🇭🇹🇭🇹🇭</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button color="default">Yo</Button>
