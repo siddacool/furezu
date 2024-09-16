6ef876ff7da268046b0aa283b9156bcff65b82ef
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-16 15:28:14 +0530

    countries pollyfill added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
+
+  polyfillCountryFlagEmojis();
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style lang="scss">
+  :global(:root) {
+    --font-family-main: 'Twemoji Country Flags', 'Montserrat', 'Roboto', 'Segoe UI', 'San Francisco',
+      sans-serif;
+  }
+
+  :global([data-theme='DARK']) {
+    color-scheme: dark;
+  }
+
+  :global(*) {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    font-family: var(--font-family-main);
+    font-optical-sizing: auto;
+    font-style: normal;
+  }
+</style>
