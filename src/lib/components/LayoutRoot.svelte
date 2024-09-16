5b4d1a66b61f9f4555696e8b7161a53bd8135a38
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-16 16:50:23 +0530

    colors fixed

--- src/lib/components/LayoutRoot.svelte
+++ src/lib/components/LayoutRoot.svelte
@@ -7,40 +7,36 @@
 
     --color-white: #fff;
 
-    /* Primary: Indigo tints */
-    --color-primary-100: #ebf4ff;
-    --color-primary-200: #c3dafe;
-    --color-primary-300: #a3bffa;
-    --color-primary-400: #7f9cf5;
-    --color-primary-500: #667eea;
-    --color-primary-600: #5a67d8;
-    --color-primary-700: #4c51bf;
-    --color-primary-800: #434190;
-    --color-primary-900: #3c366b;
+    --color-primary-100: #e8e8ff;
+    --color-primary-200: #d1d1ff;
+    --color-primary-300: #b9b9ff;
+    --color-primary-400: #a1a1ff;
+    --color-primary-500: #8989ff;
+    --color-primary-600: #7171ff;
+    --color-primary-700: #5959ff;
+    --color-primary-800: #4141ff;
+    --color-primary-900: #2929ff;
 
-    /* Grey: Grey tints */
-    --color-grey-100: #f7fafc;
-    --color-grey-200: #edf2f7;
-    --color-grey-300: #e2e8f0;
-    --color-grey-400: #cbd5e0;
-    --color-grey-500: #a0aec0;
-    --color-grey-600: #718096;
-    --color-grey-700: #4a5568;
-    --color-grey-800: #2d3748;
-    --color-grey-900: #1a202c;
+    --color-grey-100: #f2f2f2;
+    --color-grey-200: #e6e6e6;
+    --color-grey-300: #d9d9d9;
+    --color-grey-400: #cddcdc;
+    --color-grey-500: #bfbfbf;
+    --color-grey-600: #b2b2b2;
+    --color-grey-700: #a5a5a5;
+    --color-grey-800: #989898;
+    --color-grey-900: #8b8b8b;
 
-    /* Danger: Red tints */
-    --color-danger-100: #fff5f5;
-    --color-danger-200: #fed7d7;
-    --color-danger-300: #feb2b2;
-    --color-danger-400: #fc8181;
-    --color-danger-500: #f56565;
-    --color-danger-600: #e53e3e;
-    --color-danger-700: #c53030;
-    --color-danger-800: #9b2c2c;
-    --color-danger-900: #742a2a;
+    --color-danger-100: #ffebee;
+    --color-danger-200: #ffcdcd;
+    --color-danger-300: #ffb0b0;
+    --color-danger-400: #ff9494;
+    --color-danger-500: #ff7878;
+    --color-danger-600: #ff5c5c;
+    --color-danger-700: #ff4040;
+    --color-danger-800: #ff2424;
+    --color-danger-900: #ff0808;
 
-    /* Safe: Green tints */
     --color-safe-100: #f0fff4;
     --color-safe-200: #c6f6d5;
     --color-safe-300: #9ae6b4;
@@ -51,16 +47,15 @@
     --color-safe-800: #276749;
     --color-safe-900: #22543d;
 
-    /* Warning: Amber tints */
-    --color-warning-100: #fffaf0;
-    --color-warning-200: #feebc8;
-    --color-warning-300: #fbd38d;
-    --color-warning-400: #f6ad55;
-    --color-warning-500: #ed8936;
-    --color-warning-600: #dd6b20;
-    --color-warning-700: #c05621;
-    --color-warning-800: #9c4221;
-    --color-warning-900: #7b341e;
+    --color-warning-100: #fff7e8;
+    --color-warning-200: #fff2cd;
+    --color-warning-300: #ffeb94;
+    --color-warning-400: #ffdf68;
+    --color-warning-500: #ffc33c;
+    --color-warning-600: #ffb710;
+    --color-warning-700: #ffa100;
+    --color-warning-800: #e88c00;
+    --color-warning-900: #d17300;
   }
 
   :global([data-theme='DARK']) {
