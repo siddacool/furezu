e37646cf2ab268d823b72a256e68a7fac07a685f
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-20 16:11:42 +0530

    Theme settinmgs added

---
+++ src/lib/components/Settings/Appearance/ThemeSettings.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import ToolCard from '$lib/components/Settings/ToolCard.svelte';
+  import Button from '$lib/components/ui-framework/Form/Button.svelte';
+  import ButtonGroup from '$lib/components/ui-framework/Form/ButtonGroup.svelte';
+  import { useThemeStore } from '$lib/stores/local-settings/theme.svelte';
+  import { Themes } from '$lib/stores/local-settings/types';
+  import Icon from '@iconify/svelte';
+
+  function onclick(e: MouseEvent) {
+    const target = e.target as HTMLButtonElement;
+    const value = Number(target.name) as Themes;
+
+    useThemeStore.setTheme(value);
+  }
+</script>
+
+<ToolCard>
+  <div>
+    <h3>Theme</h3>
+    <ButtonGroup conjoint>
+      <Button
+        name={`${Themes.SYSTEM}`}
+        {onclick}
+        variant={useThemeStore.theme === Themes.SYSTEM ? 'primary' : 'default'}
+      >
+        Auto
+      </Button>
+      <Button
+        name={`${Themes.LIGHT}`}
+        {onclick}
+        variant={useThemeStore.theme === Themes.LIGHT ? 'primary' : 'default'}
+      >
+        Light
+      </Button>
+      <Button
+        name={`${Themes.DARK}`}
+        {onclick}
+        variant={useThemeStore.theme === Themes.DARK ? 'primary' : 'default'}
+      >
+        Dark
+      </Button>
+    </ButtonGroup>
+  </div>
+</ToolCard>
--- src/routes/settings/appearance/+page.svelte
+++ src/routes/settings/appearance/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
   import Box from '$lib/components/Box.svelte';
+  import ThemeSettings from '$lib/components/Settings/Appearance/ThemeSettings.svelte';
   import SettingsHeader from '$lib/components/Settings/SettingsHeader.svelte';
-  import ToolCard from '$lib/components/Settings/ToolCard.svelte';
-  import Button from '$lib/components/ui-framework/Form/Button.svelte';
-  import ButtonGroup from '$lib/components/ui-framework/Form/ButtonGroup.svelte';
-  import { useThemeStore } from '$lib/stores/local-settings/theme.svelte';
   import { Themes } from '$lib/stores/local-settings/types';
-
-  function onThemeChange(e: MouseEvent) {
-    const target = e.target as HTMLButtonElement;
-    const value = Number(target.name) as Themes;
-
-    useThemeStore.setTheme(value);
-  }
 </script>
 
 <title>Appearance</title>
@@ -20,26 +10,5 @@
 <SettingsHeader title="Appearance" />
 
 <Box>
-  <ToolCard>
-    <h3>Theme</h3>
-    <ButtonGroup conjoint>
-      <Button
-        name={`${Themes.SYSTEM}`}
-        onclick={onThemeChange}
-        variant={useThemeStore.theme === Themes.SYSTEM ? 'primary' : 'default'}
-      >
-        System
-      </Button>
-      <Button
-        name={`${Themes.LIGHT}`}
-        onclick={onThemeChange}
-        variant={useThemeStore.theme === Themes.LIGHT ? 'primary' : 'default'}>Light</Button
-      >
-      <Button
-        name={`${Themes.DARK}`}
-        onclick={onThemeChange}
-        variant={useThemeStore.theme === Themes.DARK ? 'primary' : 'default'}>Dark</Button
-      >
-    </ButtonGroup>
-  </ToolCard>
+  <ThemeSettings />
 </Box>
