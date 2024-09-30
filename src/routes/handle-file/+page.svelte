4642c76234926b35d03b44cbcfad29810b1a0872
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-30 12:24:38 +0530

    file added

--- src/routes/handle-file/+page.svelte
+++ src/routes/handle-file/+page.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
+  import { log } from 'console';
+
+  async function readFileAsync(files: FileList | undefined): Promise<string | null> {
+    return new Promise<string | null>((resolve, reject) => {
+      if (!files) {
+        resolve(null);
+
+        return;
+      }
+
+      if (files.length < 1) {
+        resolve(null);
+
+        return;
+      }
+
+      const file = files[0];
+
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const fileContents = event.target?.result as string;
+        resolve(fileContents);
+      };
+
+      reader.onerror = () => {
+        reject(new Error('An error occurred while reading the file.'));
+      };
+
+      reader.readAsText(file);
+    });
+  }
+
   $effect(() => {
     if ('launchQueue' in window) {
-      window?.launchQueue?.setConsumer(async (launchParams: { files: any }) => {
-        const file = launchParams.files[0];
-        const data = await file.getFile();
+      window?.launchQueue?.setConsumer(async (launchParams: { files: FileList }) => {
+        const file = await readFileAsync(launchParams.files[0]);
 
-        console.log(data);
+        console.log(file);
       });
     }
   });
