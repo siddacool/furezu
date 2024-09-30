a1595f5772474df57846ba0b99f10aeb2ea48d2b
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-30 11:43:10 +0530

    filr handlers added

---
+++ src/routes/handle-file/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  $effect(() => {
+    if ('launchQueue' in window) {
+      launchQueue.setConsumer(async (launchParams: { files: any }) => {
+        for (const file of launchParams.files) {
+          const url = URL.createObjectURL(await file.getFile());
+
+          console.log(url);
+        }
+      });
+    }
+  });
+</script>
+
+<div>yo</div>
