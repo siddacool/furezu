02652f264bd69006de69e99869409895a90b0253
Siddhesh Mangela <sidthecool007@gmail.com>
2024-09-30 12:25:14 +0530

    read file sync added

--- src/routes/handle-file/+page.svelte
+++ src/routes/handle-file/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { log } from 'console';
-
   async function readFileAsync(files: FileList | undefined): Promise<string | null> {
     return new Promise<string | null>((resolve, reject) => {
       if (!files) {
@@ -35,7 +33,7 @@
   $effect(() => {
     if ('launchQueue' in window) {
       window?.launchQueue?.setConsumer(async (launchParams: { files: FileList }) => {
-        const file = await readFileAsync(launchParams.files[0]);
+        const file = await readFileAsync(launchParams.files);
 
         console.log(file);
       });
